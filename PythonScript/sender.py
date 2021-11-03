fd5911be1103f0a2a7e4b44cdefa713d8a224f77
Katya <kotova123451234512345@gmail.com>
2021-11-03 18:28:34 +0300

    almost complete: need some error fix

--- PythonScript/sender.py
+++ PythonScript/sender.py
@@ -1,15 +1,11 @@
 import dbus
 
-print("Hello Crushnik!")
 session_bus = dbus.SessionBus()
-#names = session_bus.list_names()
-#for service in names:
+# names = session_bus.list_names()
+# for service in names:
 #    print(service)
+dbus_object = session_bus.get_object("tt.test.testName", "/tt/test/testName")
 
-dbus_object = session_bus.get_object('org.freedesktop.DBus',
-                                     '/org/freedesktop/DBus')
-dbus_iface = dbus.Interface(dbus_object, 'org.freedesktop.DBus')
-services = dbus_iface.ListNames()
-services.sort()
-for service in services:
-    print(service)
+dbus_object.Send("Hello, world!", dbus_interface = "tt.test.testName")
+
+print("Message sent")
--- dbus-echo-dir/dbus-echo.c
+++ dbus-echo-dir/dbus-echo.c
@@ -8,6 +8,7 @@ const char *send_method = "Send";
 
 void check_and_abort(DBusError *error);
 DBusHandlerResult dbus_process_message(DBusConnection *connection, DBusMessage *message, void *user_data);
+void send_call(DBusConnection *connection, DBusMessage *message);
  
 int main() {
     DBusConnection *connection = NULL;
@@ -22,13 +23,17 @@ int main() {
     dbus_bus_request_name(connection, object_interface, DBUS_NAME_FLAG_REPLACE_EXISTING, &error);
     check_and_abort(&error);
 
+    vtable.unregister_function = NULL;
+    vtable.message_function = dbus_process_message;
+
     dbus_connection_try_register_object_path(connection, "/tt/test/testName", &vtable, NULL, &error);
     check_and_abort(&error);
 
-    while (dbus_connection_read_write_dispatch(connection, -1))
+    while (dbus_connection_read_write_dispatch(connection, -1));
  
     // puts("This is my unique name");
     // puts(dbus_bus_get_unique_name(connection));
+    puts("Test is run");
     fgets(buffer, sizeof(buffer), stdin);
      
     return 0;
@@ -37,6 +42,7 @@ int main() {
 void check_and_abort(DBusError *error) {
     if (dbus_error_is_set(error)) {
         puts(error->message);
+        dbus_error_free(error);
         abort();
     }
 }
@@ -48,12 +54,33 @@ DBusHandlerResult dbus_process_message(DBusConnection *connection, DBusMessage *
 
     if ((0 == strcmp(message_interface, object_interface)) && (0 == strcmp(method_name, send_method)))
     {
-        send(connection, message);
+        puts("Process message");
+        send_call(connection, message);
         return DBUS_HANDLER_RESULT_HANDLED;
+    }
+    else
+    {
+        return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
     }
 }
 
-void send(DBusConnection *connection, DBusMessage *message)
+void send_call(DBusConnection *connection, DBusMessage *message)
 {
+    DBusError error;
+    DBusMessage *reply;
+    const char *reply_str = "Method send";
+    char *message_arg_string;
 
+    dbus_error_init(&error);
+
+    dbus_message_get_args(message, &error, DBUS_TYPE_STRING, &message_arg_string, DBUS_TYPE_INVALID);
+    puts(message_arg_string);
+    
+    reply = dbus_message_new_method_return(message);
+    // dbus_message_append_args(reply, DBUS_TYPE_STRING, reply_str, DBUS_TYPE_INVALID);
+    dbus_connection_send(connection, reply, NULL);
+
+    dbus_message_unref(reply);
+
+    puts(reply_str);
 }
